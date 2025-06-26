f2ef668aa1e4275847c98797c405d57ad2d24bb3
yogesh4617 <yogeshyogesh10514@gmail.com>
2025-06-26 16:20:38 +0530

    feat : RadialProgess theme page was updated by *v2 (version-2)

--- Frontend/src/components/ProfilePage/RadialProgess.jsx
+++ Frontend/src/components/ProfilePage/RadialProgess.jsx
@@ -10,7 +10,7 @@ const RadialProgess = ({
   console.log(progessdata);
 
   return (
-    <div className="bg-[#0f172a]/50 text-white border-[#38bdf8]/20 backdrop-blur-xl rounded-xl border hover:border-cyan-500/50 transition-all duration-300 hover:shadow-lg hover:shadow-cyan-500/10  p-4 md:px-12 lg:px-10 flex flex-col md:flex-row justify-between items-center w-full">
+    <div className="bg-cyan-800/50 text-white border-[#38bdf8]/20 backdrop-blur-xl rounded-xl border hover:border-cyan-500/50 transition-all duration-300 hover:shadow-lg hover:shadow-cyan-500/10  p-4 md:px-12 lg:px-10 flex flex-col md:flex-row justify-between items-center w-full">
       <div className="mb-4 md:mb-0">
         <h3 className="text-lg font-bold">{progessdata.missiondetails}</h3>
         <p className="text-sm text-gray-300">
@@ -19,9 +19,9 @@ const RadialProgess = ({
       </div>
       {/* For TSX uncomment the commented types below */}
       <div
-        className="radial-progress bg-gray-800 text-primary-content border-gray-800 border-4"
+        className="radial-progress bg-cyan-900/50 text-cyan-400 border-cyan-800/5 border-4"
         style={
-          { "--value": progessdata.missiondata } /* as React.CSSProperties */
+          { "--value": progessdata.missiondata ,"--thickness": "5px"} /* as React.CSSProperties */
         }
         aria-valuenow={progessdata.missiondata}
         role="progressbar"
